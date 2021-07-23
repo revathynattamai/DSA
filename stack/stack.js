7269fc7c11151bcdcf5b6ab0644dd759b8868edd
Revathy Nattamai Gopinath <Revathy.NattamaiGopinath@bestbuy.com>
2021-07-23 16:39:06 +0530

    Stacks

---
+++ stack/stack.js
@@ -0,0 +1,35 @@
+class Stack {
+    constructor() {
+        this.count = 0;
+        this.storage = {};
+    }
+
+    push(value) {
+        this.storage[this.count] = value;
+        this.count++;
+    }
+
+    pop() {
+        if(this.count === 0) return;
+        let result = this.storage[this.count -1];
+        delete this.storage[this.count -1];
+        this.count--;
+        return result;
+    }
+
+    peek() {
+        return this.storage[this.count-1];
+    }
+}
+
+let a = new Stack();
+
+a.push(1)
+a.push(2)
+console.log(a.peek())
+console.log(a.pop())
+console.log(a.peek())
+a.push("codecamp")
+console.log(a.peek())
+console.log(a.pop())
+console.log(a.peek())
